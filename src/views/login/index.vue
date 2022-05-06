226ad077c4d5e10f0d35ed718b53f8a4367d6872
丁旭东 <2459674360@qq.com>
2022-05-06 21:40:49 +0800

    feat(完善登录表单校验 + 密码框状态通用处理): 登录密码

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="login-container">
-    <el-form class="login-form">
+    <el-form class="login-form" :model="loginForm" :rules="loginRules">
       <div class="title-container">
         <h3 class="title">用户登录</h3>
       </div>
@@ -8,42 +8,62 @@
       <el-form-item prop="username">
         <span class="svg-container">
           <svg-icon icon="user" />
-          <!-- <svg-icon icon="http://res.lgdsunday.club/user.svg"></svg-icon> -->
-          <!-- <el-icon>
-            <avatar />
-          </el-icon> -->
         </span>
-        <el-input placeholder="username" name="username" type="text" />
+        <el-input placeholder="username" name="username" type="text" v-model="loginForm.username" />
       </el-form-item>
 
       <el-form-item prop="password">
         <span class="svg-container">
           <svg-icon icon="password" />
-          <!-- <el-icon>
-            <edit />
-          </el-icon> -->
         </span>
-        <el-input placeholder="password" name="password" />
+        <el-input placeholder="password" name="password" v-model="loginForm.password" :type="passwordType"/>
         <span class="show-pwd">
-          <svg-icon icon="eye" />
-          <!-- <el-icon>
-            <edit />
-          </el-icon> -->
+          <svg-icon :icon="passwordType === 'password' ? 'eye' : 'eye-open'"
+            @click="onChangePwdType" />
         </span>
       </el-form-item>
 
-      <el-button type="primary" style="width: 100%; margin-bottom: 30px"
-        >登录</el-button
-      >
+      <el-button type="primary" style="width: 100%; margin-bottom: 30px">登录</el-button>
     </el-form>
   </div>
 </template>
 
 <script setup>
-// 导入组件之后无需注册可直接使用
-// import { Avatar, Edit } from '@element-plus/icons'
-// import SvgIcon from '@/components/SvgIcon'
-import {} from 'vue'
+import { ref } from 'vue'
+import { validatePassword } from './rules'
+
+// 处理密码框文本显示状态
+const passwordType = ref('password')
+const onChangePwdType = () => {
+  if (passwordType.value === 'password') {
+    passwordType.value = 'text'
+  } else {
+    passwordType.value = 'password'
+  }
+}
+
+// 数据源
+const loginForm = ref({
+  username: 'super-admin',
+  password: '123456'
+})
+// 验证规则
+const loginRules = ref({
+  username: [
+    {
+      required: true,
+      trigger: 'blur',
+      message: '用户名为必填项'
+    }
+  ],
+  password: [
+    {
+      required: true,
+      trigger: 'blur',
+      validator: validatePassword()
+    }
+  ]
+})
 </script>
 <style lang="scss" scoped>
 $bg: #2d3a4b;
