b27f0f57260e9ef753db941aedf680f68f1d33fd
丁旭东 <2459674360@qq.com>
2022-05-08 17:35:57 +0800

    feat(生成 tagsView): tags

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -14,6 +14,8 @@
       <div class="fixed-header">
         <!-- 顶部的 navbar -->
         <navbar />
+        <!-- tags -->
+        <tags-view></tags-view>
       </div>
       <!-- 内容区 -->
       <AppMain />
@@ -24,6 +26,7 @@
 import Navbar from './components/Navbar'
 import Sidebar from './components/Sidebar'
 import AppMain from './components/AppMain'
+import TagsView from '@/components/TagsView'
 import {} from 'vue'
 import variables from '@/styles/variables.module.scss'
 </script>
